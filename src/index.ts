2daa3a35827e7d605b0db8f99888bc3ff349489b
LiraFi <iryna.lakiza@gmail.com>
2023-02-05 16:57:04 +0200

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,40 @@ import Express, {Request, Response} from "express";
 const app = Express();
 
 app.use(Express.json());
+
+const userList=[
+  {
+    id:1, userName: "Aga", age:20
+  },
+  {
+    id:2, userName: "Sam", age:25
+  },
+  {
+    id:3, userName: "Linda", age:27
+  },
+]
 //get method
 app.get("/users", (req:Request, res:Response)=>{
-  res.json("Hello!");
+  res.json(userList);
 });
 
 //post method
 app.post("/users", (req:Request, res:Response)=>{
-  
-  res.json("Create a new user.");
+  const newUser = req.body;
+  userList.push(newUser);
+  console.log(userList);
+  res.json(userList);
 });
 
 //delete method
 app.delete("/users/:id", (req:Request, res:Response)=>{
-  res.json("Delete a user with particular ID.");
+  const userId=req.params;
+  const result= userList.filter((user)=>user.id !== Number(userId.id));
+  res.json(result);
 })
 
 // assigning a port
-const port = 8000;
+const port = 8002;
 
 //listen for connection
 app.listen(port, ()=>{
